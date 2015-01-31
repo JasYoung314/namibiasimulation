cdc1da0650ab875b337eaa2a3adf4f493a5f6ccd
jasonyoung <jas.young314@gmail.com>
2015-01-31 17:00:13 +0000

    added an doctesting example

---
+++ doctesting_example.py
@@ -0,0 +1,19 @@
+"""
+A function to compute the sum of two values
+"""
+
+def sum_of_two_values(a,b):
+    """
+    Function to compute the sum of two values 
+
+    Arguments: a - The first value to be used in the sum
+               b - The second value to be used in the sum
+
+    TESTS::
+
+        >>> a,b = 2,3
+        >>> sum_of_two_values(a,b)
+        5
+    """
+
+    return a + b
